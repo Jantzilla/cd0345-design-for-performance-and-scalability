3f891d4e225541e5f309ac8c49cdb6dd3f8fcf5c
Jantz <jantzillac.93@gmail.com>
2022-11-24 00:58:00 -0600

    Create EC2 provisioning terraform main.tf

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,25 @@
-# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+    region = "us-east-1"
+    access_key = "{ACCESS KEY}"
+    secret_key = "{SECRET KEY}"
+}
 
+resource "aws_instance" "Udacity_T2" {
+    count = "4"
+    ami = "ami-0b0dcb5067f052a63"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-07bf25415476ef128"
+    tags = {
+      Name = "Udacity T2"
+    }
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
-
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "Udacity_M4" {
+    count = "2"
+    ami = "ami-0b0dcb5067f052a63"
+    instance_type = "m4.large"
+    subnet_id = "subnet-07bf25415476ef128"
+    tags = {
+      Name = "Udacity M4"
+    }
+}
