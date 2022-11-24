196472ca55b334464d2986e6774ec0776637137f
Jantz <jantzillac.93@gmail.com>
2022-11-24 01:01:15 -0600

    Add lambda ARN output variable in outputs.tf

--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,4 @@
-# TODO: Define the output variable for the lambda function.
+output "lambda_output" {
+    value = aws_lambda_function.my_lambda.arn
+    description = "A list of the lambda function permissions"
+}
